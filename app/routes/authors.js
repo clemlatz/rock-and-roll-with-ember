46847d3b69d35ee2cfbfa9753862ebeb76600ad1
Clément Bourgoin <c@iwzr.fr>
2021-02-14 21:09:54 +0100

    Add Author model

--- app/routes/authors.js
+++ app/routes/authors.js
@@ -1,4 +1,26 @@
 import Route from '@ember/routing/route';
+import { tracked } from '@glimmer/tracking';
+
+class Author {
+  @tracked firstName;
+  @tracked lastName;
+
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  get name() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
 
 export default class AuthorsRoute extends Route {
+  model() {
+    return [
+      new Author('Robert Charles', ' Wilson'),
+      new Author('Greg', 'Egan'),
+      new Author('Ted', 'Chiang'),
+    ];
+  }
 }
